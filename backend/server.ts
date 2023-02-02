bdb65c8c20e48113710c88f445dc4cc2ad8110e2
JameEnder <47925045+JameEnder@users.noreply.github.com>
2023-02-02 18:07:53 +0100

    Change server.ts port to 5000

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Listening at port 3000")
+app.listen(5000, () => {
+    console.log("Listening at port 5000")
 });
