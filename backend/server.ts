caba7775819c4db907ecd75a6bd1b4f79ad943fc
Biswajeeban <bishwajeeban.mishra@gmail.com>
2023-02-09 01:35:19 +0530

    cors package impoprted

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
+
 import { foxRouter } from './api/foxes';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', [foxRouter])
 
