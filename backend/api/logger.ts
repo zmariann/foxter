42376cb33083af13385dd2772a50e3f9b841cde0
Zenka1986 <107252094+Zenka1986@users.noreply.github.com>
2023-02-09 18:07:36 +0100

    adding timestamp in logger

--- backend/api/logger.ts
+++ backend/api/logger.ts
@@ -3,7 +3,8 @@ import express from "express";
 const loggerApp = express()
 
 function logger(request, response, next) {
-    console.log(`${request.method} url: ${request.url}`);
+    let timeStart = new Date().getTime()
+    console.log(`${timeStart} ${request.method} url: ${request.url}`);
     next()
 }
 
