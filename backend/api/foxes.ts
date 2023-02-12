99cbbd1da5d466717eda08e3ab4b7c41775c946f
JameEnder <47925045+JameEnder@users.noreply.github.com>
2023-02-12 18:40:25 +0100

    Reduced comments to only necessary

--- backend/api/foxes.ts
+++ backend/api/foxes.ts
@@ -3,18 +3,13 @@ import { db } from "../database/db";
 
 const foxRouter = express.Router();
 
-// Retrieve all foxes from the database and send as a response
 foxRouter.get("/foxes", (req, res) => {
-  // Fetch data from the foxes table, ordered by the newest entries first
   const data = db.prepare("SELECT * FROM foxes ORDER BY id DESC").all();
-  // Return the data as the response
   res.send(data);
 });
 
-// Add a new fox to the database
 foxRouter.post("/foxes", (req, res) => {
     try {
-      // Extract the content from the request body
       const { content } = req.body;
     
       // Return an error if the content is missing
@@ -22,29 +17,22 @@ foxRouter.post("/foxes", (req, res) => {
         return res.status(400).send({ error: "Content is required." });
       }
     
-      // Prepare an INSERT statement to add the fox to the database
       const stmt = db.prepare("INSERT INTO foxes (content) VALUES (?)");
     
-      // Run the statement
       stmt.run(content);
     
-      // Return a success message
       res.status(201).send({ message: "Fox created successfully!" });
     } catch (error) {
-      // Return the error message if an error occurs
       res.status(400).send({ error: error.message });
     }  
 });
   
-// Delete a fox from the database
 foxRouter.delete('/foxes/:id', async (req, res) => {
   try {
     // Prepare a DELETE statement to remove the fox with the specified id
     db.prepare("DELETE FROM foxes WHERE id = ?").run(req.params.id);
-    // Return a success message
     res.status(200).send('Entry deleted');
   } catch (error) {
-    // Return the error message if an error occurs
     res.send(error.message);
   }
 })
--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,3 @@
-// Import express framework
 import express from "express";
 
 // Import cors to handle cross-origin resource sharing
@@ -7,11 +6,9 @@ import cors from "cors";
 // Load environment variables from .env file
 require("dotenv").config();
 
-// Import foxRouter and loggerMiddleware from the api directory
 import { foxRouter } from "./api/foxes";
 import { loggerMiddleware } from "./api/logger";
 
-// Create an express application
 const app = express();
 
 // Get the port number from environment variable or use 5000 as default
@@ -23,13 +20,10 @@ app.use(cors());
 // Use json middleware for parsing incoming request bodies as JSON
 app.use(express.json());
 
-// Use loggerMiddleware to log request and response data
 app.use(loggerMiddleware);
 
-// Use foxRouter for handling fox-related requests/ / Mount the foxes router on the /api endpoint
 app.use("/api", [foxRouter]);
 
-// start the server and Listen for incoming connections on the specified port
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
 });
