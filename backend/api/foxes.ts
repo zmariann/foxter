b611545d16234cb1f7bb746a07fe18aacec88bc6
bishwajeeban <bishwajeeban.mishra@gmail.com>
2023-02-17 18:09:26 +0530

    verify user token function called in create a fox

--- backend/api/foxes.ts
+++ backend/api/foxes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { db } from "../database/db";
+import { verifyUser } from "./auth";
 
 const foxRouter = express.Router();
 
@@ -8,25 +9,36 @@ foxRouter.get("/foxes", (req, res) => {
   res.send(data);
 });
 
+// create a fox
 foxRouter.post("/foxes", (req, res) => {
-    try {
-      const { content } = req.body;
-    
-      // Return an error if the content is missing
-      if (!content) {
-        return res.status(400).send({ error: "Content is required." });
-      }
-    
-      const stmt = db.prepare("INSERT INTO foxes (content) VALUES (?)");
-    
-      stmt.run(content);
-    
-      res.status(201).send({ message: "Fox created successfully!" });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }  
+  try {
+    const { content } = req.body;
+
+    // Return an error if the content is missing
+    if (!content) {
+      return res.status(400).send({ error: "Content is required." });
+    }
+
+    const token = req.cookies.token;
+
+    // Verify the user token
+    const user = verifyUser(token);
+
+    if (!user) {
+      return res.status(401).send({ error: "Unauthorized." });
+    }
+
+    const stmt = db.prepare("INSERT INTO foxes (content) VALUES (?)");
+
+    stmt.run(content);
+
+    res.status(201).send({ message: "Fox created successfully!" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
   
+// Delete a fox
 foxRouter.delete('/foxes/:id', async (req, res) => {
   try {
     // Prepare a DELETE statement to remove the fox with the specified id
