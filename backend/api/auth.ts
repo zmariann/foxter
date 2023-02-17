7ea23b8476674d29f5e4135a340eac76f978106c
zmariann <zaszlosmariann@gmail.com>
2023-02-17 17:23:24 +0100

    auth.ts

--- backend/api/auth.ts
+++ backend/api/auth.ts
@@ -1,78 +1,15 @@
-import express from 'express';
-// npm install cookie-parser
-const cookieParser = require('cookie-parser');
-// npm install jsonwebtoken
-const jwt = require('jsonwebtoken')
-const auth = express();
-// npm install bcrypt
-const bcrypt = require('bcrypt');
-
-auth.use(cookieParser());
-
-// / TOKEN -> Username const TOKENS = new Map<string, string>()
-// function generateToken() { 
-// return "32-random-characters" 
-// } 
-
-// fire a function before doc saved to db
-
-
-const tokens = new Map<string, string>();
-tokens.set('Marika', 'secret passw');
-
-async function generateToken() {
-    const passw = 'password';
-    const hashedpassw = await bcrypt.hash(passw, 10)
-    const result = await bcrypt.compare(passw, hashedpassw)
-}
-
-/*
-Jakub's code for help:
-
-app.post("/api/login", (req, res) => { 
-
-const { username, password } = req.body;
-
-validate(username) 
-
-validate(password) 
-
-const token = generateToken() 
-
-TOKENS.set(token, username) return res.status(200).send(token) 
-
-}) 
-
-app.get("/api/secret", (req, res) => { 
-
-const username: string | undefined = TOKENS.get(req.headers["X-Auth-Token"]) 
-
-if (username === undefined) { 
-
-return res.status(401).send("Not authorized") 
-
-} 
-
-const secret = getSecretFor(username) 
-
-return res.send(secret) }) 
-
-app.post("/api/logout", (req, res) => 
-
-{ const { token } = req.headers["X-Auth-Token"] TOKENS.remove(token) return res.send("Succesfuly logout") 
-
-})
-*/
-
-// Biswa's code
-
-/*
 import express, { Request, Response } from "express";
 import { db } from "../database/db";
+// npm install cookie-parser
+const cookieParser = require("cookie-parser");
+// npm install jsonwebtoken
+import jwt from "jsonwebtoken";
+// npm install bcrypt
 import bcrypt from "bcrypt";
-//import jwt from "jsonwebtoken";
 
 const authRouter = express.Router();
+
+authRouter.use(cookieParser());
 
 // Generate a random token
 function generateToken(): string {
@@ -156,4 +93,4 @@ function verifyUser(req: Request): { name: string } | null | boolean {
 }
 
 export { authRouter, verifyUser };
-*/
+
