1d4ccc3420b6b12a7cdf18638111d6ad7d2eea53
bishwajeeban <bishwajeeban.mishra@gmail.com>
2023-03-27 15:11:57 +0530

    Fox component added

--- frontend/src/components/Fox.tsx
+++ frontend/src/components/Fox.tsx
@@ -1,44 +1,13 @@
-// import type {FoxProps as FoxInterface} from '../../../shared/types'
-// import {NextPage} from 'next'
-
-// const Fox: NextPage <{props: FoxInterface}> = (props) => {
-//     return (
-//         <>
-//         <p>{props.props.content}</p>
-//         </>
-//     )
-// }
-
-// const fox = {id: 0 , content: "Hello"};
-// <div>
-//     <Fox props={fox}></Fox>
-// </div>
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import LikeButton from "../components/LikeButton";
 import type { FoxProps } from "../../../shared/types";
 
+interface Props {
+  fox: FoxProps;
+  onDeleteFox: (id: number) => void;
+}
 
-const FoxForm: React.FC<FoxProps> = ({ foxes, onAddFox, onDeleteFox }) => {
-  const [text, setText] = useState("");
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    try {
-      await fetch("/api/foxes", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ content: text }),
-      });
-      setText("");
-      onAddFox(text);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+const Fox: React.FC<Props> = ({ fox, onDeleteFox }) => {
   const handleDelete = async (id: number) => {
     try {
       await fetch(`/api/foxes/${id}`, {
@@ -52,38 +21,18 @@ const FoxForm: React.FC<FoxProps> = ({ foxes, onAddFox, onDeleteFox }) => {
 
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Content:
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
+      <li key={fox.id} style={{ margin: "20px 0" }}>
+        {fox.content}
+        <span style={{ marginLeft: "10px" }}></span>
 
-                 <ul style={{ listStyle: "none", textAlign: "center" }}>
-                  {/* Map through foxes array and render fox content and delete button */}
-                  {foxes.map((fox) => {
-                    return (
-                      <li key={fox.id} style={{ margin: "20px 0" }}>
-                        {fox.content}
-                        <span style={{ marginLeft: "10px" }}></span>
+        {/* like Button */}
+        <LikeButton foxId={fox.id} initialLikes={fox.likes} />
 
-                        {/* like Button */}
-                        <LikeButton foxId={fox.id} initialLikes={fox.likes} />
-
-                        {/* Delete Button */}
-                        <button onClick={() => handleDelete(fox.id)}>
-                          Delete
-                        </button>
-                      </li>
-                    );
-                  })}
-                </ul>
-        
-      </div>
+        {/* Delete Button */}
+        <button onClick={() => handleDelete(fox.id)}>Delete</button>
+      </li>
     </>
   );
 };
 
-export default FoxForm;
+export default Fox;
