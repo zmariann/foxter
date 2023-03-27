752f0024becac740f9cebbe3664de3af262c37f3
bishwajeeban <bishwajeeban.mishra@gmail.com>
2023-03-27 15:13:55 +0530

    FoxProps types declared

--- shared/types.d.ts
+++ shared/types.d.ts
@@ -1,10 +1,7 @@
 interface FoxProps {
-    id: number;
-    content: string;
-    created_at: Date;
-    likes: number;
-    foxes: Fox[];
-    onAddFox: (content: string) => void;
-    onDeleteFox: (id: number) => void;
-  }
-export {FoxProps}  
+  id: number;
+  content: string;
+  created_at: Date;
+  likes: number;
+}
+export { FoxProps };
