73a4825014aae63ce3526c38c46216fd97bb4308
bishwajeeban <bishwajeeban.mishra@gmail.com>
2023-03-29 20:54:10 +0530

    Fox Components updated

--- frontend/src/components/Fox.tsx
+++ frontend/src/components/Fox.tsx
@@ -1,24 +1,30 @@
+// Including necessary React Packages, LikeComponent and Types
 import React from "react";
 import LikeButton from "../components/LikeButton";
 import type { FoxProps } from "../../../shared/types";
 
+// Defining Fox component  property
 interface FProps {
   fox: FoxProps;
   onDeleteFox: (id: number) => void;
 }
 
+// Creating the Fox Component
 const Fox: React.FC<FProps> = ({ fox, onDeleteFox }) => {
+  // Method to delete a fox
   const handleDelete = async (id: number) => {
     try {
+      // Sending delete request to the API to delete the fox
       await fetch(`/api/foxes/${id}`, {
         method: "DELETE",
       });
+      // Removing the deleted fox from the list
       onDeleteFox(id);
     } catch (error) {
       console.error(error);
     }
   };
-
+// rendering Fox Component 
   return (
     <>
       <li className="bg-white p-6 rounded shadow mb-8 flex flex-col max-w-xl">
