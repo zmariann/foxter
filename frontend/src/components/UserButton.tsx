2e05a88889d1e2b62c4dda5ee1e0bb28962e8e3e
bishwajeeban <bishwajeeban.mishra@gmail.com>
2023-05-03 23:49:57 +0530

    Fox-73/76 test for post message api  created

--- frontend/src/components/UserButton.tsx
+++ frontend/src/components/UserButton.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
+import Link from "next/link";
 
 export default function UserButton() {
   const [user, setUser] = useState({
@@ -28,7 +29,7 @@ export default function UserButton() {
 
   return user.name ? (
     <>
-      <div className="flex-shrink-0 flex hover:bg-blue-00 rounded-full p-4 mt-12 mr-2 fixed left-2 bottom-0">
+      <div className="flex-shrink-0 flex hover:bg-blue-00 rounded-full p-4 mt-12 mr-2 fixed left-2 bottom-0 cursor-pointer">
         <a className="flex-shrink-0 group block" target="_blank">
           <div className="flex items-center">
             <div>
@@ -64,8 +65,9 @@ export default function UserButton() {
                 <MenuItem>
                   <a href="/logout">Logout</a>
                 </MenuItem>
-                <MenuItem>Profile</MenuItem>
-                <MenuItem>Add Another Account</MenuItem>
+                <MenuItem>
+                  <Link href={`/profiles/${user.name}`}>Profile</Link>
+                </MenuItem>
               </Menu>
             </div>
           </div>
@@ -73,6 +75,15 @@ export default function UserButton() {
       </div>
     </>
   ) : (
-    <></>
+    <>
+      <div className="flex-1">
+        <a
+          href="/login"
+          className="bg-green-400 mt-5 hover:bg-green-600 text-white font-bold py-2 px-8 rounded-full mr-8 float-right"
+        >
+          Login
+        </a>
+      </div>
+    </>
   );
 }
