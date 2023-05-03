b05279a91fe91def9da2c2bc46140f53c9e9557c
bishwajeeban <bishwajeeban.mishra@gmail.com>
2023-05-03 21:36:09 +0530

    Fox-73/76: tests for /api/rooms and related paths

---
+++ backend/tests/messages.test.ts
@@ -0,0 +1,55 @@
+import request from "supertest";
+import { app } from '../app'
+
+let token;
+let headers;
+beforeAll(async () => {
+  const response = await request(app)
+    .post("/api/login")
+    .send({ name: "test", password: "test" });
+  headers = response.headers;
+});
+
+describe("Rooms API", () => {
+  let roomId = null;
+  let roomName = "Test Room";
+
+  // Test case for GET /api/rooms
+  test("GET /api/rooms - Retrieves all rooms", async () => {
+    const response = await request(app)
+      .get("/api/rooms")
+      .set("Cookie", [...headers["set-cookie"]]);
+    expect(response.status).toEqual(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+  });
+
+  // Test case for POST /api/rooms
+  test("POST /api/rooms - Creates a new room", async () => {
+    const roomData = { name: roomName, group: 1 };
+    const response = await request(app).post("/api/rooms").set("Cookie", [...headers["set-cookie"]]).send(roomData);
+    roomId = response.body.roomId
+    expect(response.status).toEqual(200);
+    });
+
+  // Test case for GET /api/rooms/:id
+  test("GET /api/rooms/:id - Retrieves a specific room", async () => {
+    const response = await request(app).get(`/api/rooms/${roomId}`).set("Cookie", [...headers["set-cookie"]]);
+    expect(response.status).toEqual(200);
+    expect(response.body).toStrictEqual([]);
+  });
+
+  // Test case for GET /api/rooms/name/:id
+  test("GET /api/rooms/name/:id - Retrieves name of a specific room", async () => {
+    const response = await request(app).get(`/api/rooms/name/${roomId}`).set("Cookie", [...headers["set-cookie"]]);
+    expect(response.status).toEqual(200);
+    expect(response.body.name).toBe(roomName);
+  });
+
+  // Test case for DELETE /api/rooms/:id
+  test("DELETE /api/rooms/:id - Deletes a specific room", async () => {
+    const response = await request(app).delete(`/api/rooms/${roomId}`).set("Cookie", [...headers["set-cookie"]]);
+
+    expect(response.status).toEqual(200);
+    expect(response.body.message).toBe("Room is successfully deleted.");
+  });
+});
