6590de67358f760b2bd47246785b6b1c200488ae
JameEnder <47925045+JameEnder@users.noreply.github.com>
2023-05-05 18:10:43 +0200

    Update db.ts

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -1,8 +1,8 @@
 import fs from 'fs'
-import sqlite3 from 'better-sqlite3'
+import sqlite from 'better-sqlite3'
 
 const name = process.env['NODE_ENV'] == 'test' ? 'test' : 'foxter'
-const db: any = sqlite3(name + '.db')
+const db: any = sqlite(name + '.db')
 
 db.exec(fs.readFileSync('./database/create.sql').toString())
 
