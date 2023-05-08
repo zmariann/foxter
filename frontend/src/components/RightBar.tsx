87d156580bd021a21494f9a79ba4946f259e0273
Zenka1986 <107252094+Zenka1986@users.noreply.github.com>
2023-05-08 17:19:52 +0200

    search feature

--- frontend/src/components/RightBar.tsx
+++ frontend/src/components/RightBar.tsx
@@ -1,16 +1,53 @@
+import React, { useEffect, useState } from "react";
+
 export default function RightBar() {
+  const [input, setInput] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const fetchData = (value) => {
+    fetch("/api/foxes")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((fox) => {
+          return (
+            fox && fox.content && fox.content.toLowerCase().includes(value)
+          );
+        });
+        console.log(results);
+        setSearchResult(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    if (value.length < 3) {
+      setSearchResult([]);
+    } else {
+      fetchData(value);
+    }
+  };
+
   return (
     <>
       <div className={`w-3/12 h-screen bg-white mt-4 px-2`}>
-        <div>
+        <div className="flex items-center">
+          <img src="Search"></img>
           <input
-            className="focus:ring-2 focus:ring-offset-lime-300 outline-none rounded-2xl w-full pl-5 p-8"
+            className="focus:ring-2 focus:ring-offset-lime-300 outline-none rounded-2xl w-full pl-5 p-5"
             type="text"
             placeholder="Search foxes"
+            value={input}
+            onChange={(e) => handleChange(e.target.value)}
           />
         </div>
-        <div className="w-full max-w-md m-auto rounded-2xl bg-whiteFox shadow-md p-5 mt-5">
-          Trends
+        <div className="w-full max-w-md m-auto rounded-2xl bg-whiteFox shadow-md p-5 mt-5 flex-col place-items-center">
+          <img src="logo"></img> Foxter
+          {input.length > 0 && input.length < 3 ? (
+            <div>Enter at least 3 characters</div>
+          ) : (
+            searchResult.map((result) => {
+              return <div key={result.id}>{result.content}</div>;
+            })
+          )}
         </div>
         <div className="w-full max-w-md m-auto rounded-2xl bg-whiteFox shadow-md p-5 mt-5">
           Who to follow
