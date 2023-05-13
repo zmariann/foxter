1404cdcec6d8f7c04c2b122540707671d742dc53
JameEnder <47925045+JameEnder@users.noreply.github.com>
2023-05-13 18:18:32 +0200

    Added a warning about JWT_SECRET

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,11 @@ import { app } from "./app";
 // Get the port number from environment variable or use 5000 as default
 const PORT = process.env.PORT || 5000;
 
+if (process.env.JWT_SECRET == undefined) {
+  console.error("You need to set the JWT_SECRET env variable");
+  process.exit(1);
+}
+
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
 
